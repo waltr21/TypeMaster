a34976d16f80e845b6f18dfb5c66306cf94cb7bf
hunterhubers <hunterhubers@gmail.com>
2016-04-19 15:52:04 -0400

    blah 5

--- app/build/generated/source/r/debug/edu/gvsu/cis/waltr/typemaster/R.java
+++ app/build/generated/source/r/debug/edu/gvsu/cis/waltr/typemaster/R.java
@@ -1741,7 +1741,7 @@ containing a value of this type.
         public static final int notification_template_icon_bg=0x7f020045;
     }
     public static final class id {
-        public static final int action0=0x7f0c0062;
+        public static final int action0=0x7f0c0063;
         public static final int action_bar=0x7f0c0042;
         public static final int action_bar_activity_content=0x7f0c0000;
         public static final int action_bar_container=0x7f0c0041;
@@ -1750,7 +1750,7 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f0c0023;
         public static final int action_bar_title=0x7f0c0022;
         public static final int action_context_bar=0x7f0c0043;
-        public static final int action_divider=0x7f0c0066;
+        public static final int action_divider=0x7f0c0067;
         public static final int action_menu_divider=0x7f0c0002;
         public static final int action_menu_presenter=0x7f0c0003;
         public static final int action_mode_bar=0x7f0c003f;
@@ -1761,9 +1761,9 @@ containing a value of this type.
         public static final int always=0x7f0c001c;
         public static final int beginning=0x7f0c0019;
         public static final int buttonPanel=0x7f0c002c;
-        public static final int cancel_action=0x7f0c0063;
+        public static final int cancel_action=0x7f0c0064;
         public static final int checkbox=0x7f0c003a;
-        public static final int chronometer=0x7f0c005e;
+        public static final int chronometer=0x7f0c005f;
         public static final int collapseActionView=0x7f0c001d;
         public static final int contentPanel=0x7f0c0032;
         public static final int custom=0x7f0c0038;
@@ -1771,10 +1771,10 @@ containing a value of this type.
         public static final int decor_content_parent=0x7f0c0040;
         public static final int default_activity_button=0x7f0c0028;
         public static final int disableHome=0x7f0c000d;
-        public static final int editText=0x7f0c005f;
+        public static final int editText=0x7f0c0060;
         public static final int edit_query=0x7f0c0044;
         public static final int end=0x7f0c001a;
-        public static final int end_padder=0x7f0c006d;
+        public static final int end_padder=0x7f0c006e;
         public static final int expand_activities_button=0x7f0c0026;
         public static final int expanded_menu=0x7f0c0039;
         public static final int gameSummary=0x7f0c0053;
@@ -1784,13 +1784,13 @@ containing a value of this type.
         public static final int icon=0x7f0c002a;
         public static final int ifRoom=0x7f0c001e;
         public static final int image=0x7f0c0027;
-        public static final int info=0x7f0c006c;
+        public static final int info=0x7f0c006d;
         public static final int item_touch_helper_previous_elevation=0x7f0c0005;
-        public static final int line1=0x7f0c0067;
-        public static final int line3=0x7f0c006a;
+        public static final int line1=0x7f0c0068;
+        public static final int line3=0x7f0c006b;
         public static final int listMode=0x7f0c000a;
         public static final int list_item=0x7f0c0029;
-        public static final int media_actions=0x7f0c0065;
+        public static final int media_actions=0x7f0c0066;
         public static final int middle=0x7f0c001b;
         public static final int minute_card=0x7f0c0051;
         public static final int multiply=0x7f0c0014;
@@ -1804,7 +1804,7 @@ containing a value of this type.
         public static final int progress_circular=0x7f0c0006;
         public static final int progress_horizontal=0x7f0c0007;
         public static final int radio=0x7f0c003c;
-        public static final int randomWord=0x7f0c005d;
+        public static final int randomWord=0x7f0c005e;
         public static final int scoreSummary=0x7f0c005c;
         public static final int scoreTitle=0x7f0c005b;
         public static final int score_card=0x7f0c005a;
@@ -1827,20 +1827,21 @@ containing a value of this type.
         public static final int showCustom=0x7f0c0010;
         public static final int showHome=0x7f0c0011;
         public static final int showTitle=0x7f0c0012;
+        public static final int signIn=0x7f0c005d;
         public static final int spacer=0x7f0c002d;
         public static final int split_action_bar=0x7f0c0008;
         public static final int src_atop=0x7f0c0016;
         public static final int src_in=0x7f0c0017;
         public static final int src_over=0x7f0c0018;
-        public static final int status_bar_latest_event_content=0x7f0c0064;
+        public static final int status_bar_latest_event_content=0x7f0c0065;
         public static final int submit_area=0x7f0c004d;
         public static final int tabMode=0x7f0c000c;
-        public static final int text=0x7f0c006b;
-        public static final int text2=0x7f0c0069;
+        public static final int text=0x7f0c006c;
+        public static final int text2=0x7f0c006a;
         public static final int textSpacerNoButtons=0x7f0c0035;
-        public static final int textView3=0x7f0c0060;
-        public static final int textView4=0x7f0c0061;
-        public static final int time=0x7f0c0068;
+        public static final int textView3=0x7f0c0061;
+        public static final int textView4=0x7f0c0062;
+        public static final int time=0x7f0c0069;
         public static final int title=0x7f0c002b;
         public static final int title_template=0x7f0c0030;
         public static final int topPanel=0x7f0c002f;
@@ -1927,12 +1928,16 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f060011;
         public static final int abc_toolbar_collapse_description=0x7f060012;
         public static final int app_name=0x7f060014;
-        public static final int howMany=0x7f060015;
-        public static final int minute=0x7f060016;
-        public static final int practice=0x7f060017;
+        public static final int highScoreSub=0x7f060015;
+        public static final int high_score=0x7f060016;
+        public static final int howMany=0x7f060017;
+        public static final int minute=0x7f060018;
+        public static final int practice=0x7f060019;
+        public static final int practice_sub=0x7f06001a;
+        public static final int sign_in=0x7f06001b;
         public static final int status_bar_notification_info_overflow=0x7f060013;
-        public static final int subMinute=0x7f060018;
-        public static final int subhowMany=0x7f060019;
+        public static final int subMinute=0x7f06001c;
+        public static final int subhowMany=0x7f06001d;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat=0x7f09007d;
