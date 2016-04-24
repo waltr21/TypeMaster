3dd5980f2123c64fe03dad0c652ad716293fe8ef
hunterhubers <hunterhubers@gmail.com>
2016-04-24 14:30:01 -0400

    Finished Sign In and Sign out button.

--- app/src/main/java/edu/gvsu/cis/waltr/typemaster/Main.java
+++ app/src/main/java/edu/gvsu/cis/waltr/typemaster/Main.java
@@ -3,6 +3,7 @@ package edu.gvsu.cis.waltr.typemaster;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -19,7 +20,9 @@ import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.data.Freezable;
 import com.google.android.gms.games.Games;
 import com.google.android.gms.games.leaderboard.Leaderboard;
-import com.google.example.games.basegameutils.BaseGameUtils;
+
+import com.google.android.gms.common.SignInButton;
+
 
 
 
@@ -34,7 +37,8 @@ public class Main extends AppCompatActivity implements GoogleApiClient.Connectio
     private static int RC_SIGN_IN = 9001;
     private static int REQUEST_LEADERBOARD = 0;
     private CardView minuteCard, wordCard, practiceCard, scoreCard;
-    private Button signIn, signOut;
+    private SignInButton signIn;
+    private Button signOut;
     private boolean wordGame, minuteGame;
     private boolean mResolvingConnectionFailure = false;
     private boolean mAutoStartSignInFlow = true;
@@ -43,7 +47,14 @@ public class Main extends AppCompatActivity implements GoogleApiClient.Connectio
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(mResolvingConnectionFailure){
+                mResolvingConnectionFailure = false;
+                mGoogleApiClient.connect();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
     }
 
 
@@ -81,7 +92,7 @@ public class Main extends AppCompatActivity implements GoogleApiClient.Connectio
         practiceCard = (CardView) findViewById(R.id.practice_card);
         scoreCard = (CardView) findViewById(R.id.score_card);
 
-        signIn = (Button) findViewById(R.id.button2);
+        signIn = (SignInButton) findViewById(R.id.button2);
         signOut = (Button) findViewById(R.id.button3);
 
 
@@ -91,6 +102,7 @@ public class Main extends AppCompatActivity implements GoogleApiClient.Connectio
 
         // Create the Google Api Client with access to Plus and Games
         mGoogleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .addConnectionCallbacks(this)
                 .addOnConnectionFailedListener(this)
                 .addApi(Games.API).addScope(Games.SCOPE_GAMES)
                 // add other APIs and scopes here as needed
@@ -145,45 +157,34 @@ public class Main extends AppCompatActivity implements GoogleApiClient.Connectio
         signIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View press) {
-               if (!mInSignInFlow && !mExplicitSignOut) {
-                    // auto sign in
-                   mGoogleApiClient.connect();
-               }
+                mSignInClicked = true;
+                mGoogleApiClient.connect();
             }
         });
 
         signOut.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View press) {
+                mSignInClicked = false;
+                if(mGoogleApiClient.isConnected()){
+                    Games.signOut(mGoogleApiClient);
+                }
                 mGoogleApiClient.disconnect();
+
+                //Show and Hide sign in and out buttons.
+
+                signIn.setVisibility(View.VISIBLE);
+                signOut.setVisibility(View.GONE);
             }
         });
 
     }
 
-
-
-
-      // initialized in onCreate
-
-    /*@Override
-    protected void onStart() {
-        super.onStart();
-        if (!mInSignInFlow && !mExplicitSignOut) {
-            // auto sign in
-            mGoogleApiClient.connect();
-        }
-    }*/
-
-    /*@Override
-    protected void onStop() {
-        super.onStop();
-        mGoogleApiClient.disconnect();
-    }*/
-
     @Override
     public void onConnected(@Nullable Bundle bundle) {
         scoreCard.setEnabled(true);
+        signIn.setVisibility(View.GONE);
+        signOut.setVisibility(View.VISIBLE);
         // show sign-out button, hide the sign-in button
         // (your code here: update UI, enable functionality that
     }
@@ -202,19 +203,22 @@ public class Main extends AppCompatActivity implements GoogleApiClient.Connectio
 
         // If the sign in button was clicked or if auto sign-in is enabled,
         // launch the sign-in flow
-        if (mSignInClicked || mAutoStartSignInFlow) {
-            mAutoStartSignInFlow = false;
+        if (mSignInClicked) {
             mSignInClicked = false;
             mResolvingConnectionFailure = true;
 
-            // Attempt to resolve the connection failure using BaseGameUtils.
-            // The R.string.signin_other_error value should reference a generic
-            // error string in your strings.xml file, such as "There was
-            // an issue with sign in, please try again later."\
-            String errorString = getResources().getString(R.string.signin_other_error);
-            if (!BaseGameUtils.resolveConnectionFailure(this,
-                    mGoogleApiClient, connectionResult, RC_SIGN_IN, errorString)) {
-                mResolvingConnectionFailure = false;
+            if(connectionResult.hasResolution()){
+                try{
+                    connectionResult.startResolutionForResult(this, RC_SIGN_IN);
+                    mResolvingConnectionFailure = true;
+                } catch (IntentSender.SendIntentException e) {
+                    e.printStackTrace();
+                    mResolvingConnectionFailure = false;
+                    mGoogleApiClient.connect();
+                }
+            } else {
+                //FIXME
+                //Snackbar.make(signOut, connectionResult.getErrorMessage(), Snackbar.LENGTH_LONG).show();
             }
         }
 
